b0ebd0413062d39f1924cb8292e9445c5e696ab5
SarumathiV02 <sarumathi@sparkouttech.com>
2024-08-05 16:45:47 +0530

    login works

--- User/signuplogin.spec.js
+++ User/signuplogin.spec.js
@@ -2,28 +2,36 @@ const {test, expect}= require('@playwright/test');
 const signup = require('./signup');
 const login = require('./login')
 
-test('To verify the page will redirected to the signup page when the user click the signup element in AI Interview page.', async ({ page }) => {
+// test('To verify the page will redirected to the signup page when the user click the signup element in AI Interview page.', async ({ page }) => {
     
-    const signUpPage = new signup(page);
+//     const signUpPage = new signup(page);
 
-    await page.goto("https://ai-interview-frontend.seaswap.co/signup");
+//     await page.goto("https://ai-interview-frontend.seaswap.co/signup");
 
-    await expect(page).toHaveTitle('Hireko.ai');
+//     await expect(page).toHaveTitle('Hireko.ai');
 
-    await signUpPage.NewUserLogin('Sarumathi','Vasu','Sarumathi_Vasu','sarumathitvm02@gmail.com','V_saru2002');
+//     await signUpPage.NewUserLogin('Sarumathi','Vasu','Sarumathi_Vasu','sarumathitvm02@gmail.com','V_saru2002');
 
-    await signUpPage.checkToastrMessage("Email Already Registered", "full url"); //checks if the user is already registered.
+//     await signUpPage.checkToastrMessage("Email already exist"); //checks if the user is already registered.
 
 test('To verify the user can log in on AI Interview page.', async ({ page }) => {
 
-    const loginPage = new Login(page);
+    // const isalreadyRegistered = await signUpPage.checkToastrMessage("Email already exist");
+
+    // if(isalreadyRegistered){
+
+        // const loginpage = new login(page);
+
+        await page.goto("https://ai-interview-frontend.seaswap.co/login");
     
-    await page.goto("https://ai-interview-frontend.seaswap.co/login");
+        await expect(page).toHaveTitle('Hireko.ai');
+        
+        await loginpage.UserLogin('sarumathitvm02@gmail.com', 'V_saru2002');
+
+    // }
     
-    await expect(page).toHaveTitle('Hireko.ai');
-    
-    await loginPage.UserLogin('sarumathitvm02@gmail.com', 'V_saru2002');
+
 });
 
-await browser.close();
-});
+// await browser.close();
+// });
